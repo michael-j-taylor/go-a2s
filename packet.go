729fc3ad8b2790b53fd1d86f3e8db4a8c9beb67b
michael-j-taylor <michaeljtaylor15@gmail.com>
2020-10-03 17:25:34 -0400

    further increased buffer size

--- packet.go
+++ packet.go
@@ -10,7 +10,7 @@ import (
 
 // Note that some games such as squad may use a non-standard packet size
 // Refer to the game documentation to see if this needs to be changed
-const MaxPacketSize = 2800 //doubled for Rust
+const MaxPacketSize = 5600 //quadroupled for Rust
 
 var (
 	ErrOutOfBounds = errors.New("Read out of bounds")
